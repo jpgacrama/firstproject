0c469aded12a762e58d298004a38e5515fd127b0
Jonas Gacrama <gacrama.jonas@gmail.com>
2022-02-19 18:43:55 +0800

    Created a new route where an ID is supplied to the route

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,10 +7,22 @@ use Illuminate\Http\Request;
 class ProductsController extends Controller
 {
     public function index() {
-        return view('products.index');
+        $title = 'Welcome to my Laravel Course';
+        $description = 'Created by Jonas';
+
+        $data = [
+            'productOne' => 'iPhone',
+            'productTwo' => 'Samsung'
+        ];
+        
+        return view('products.index')->with('data', $data);
     }
 
     public function about() {
         return 'About Us page';
+    }
+
+    public function show($id) {
+        return $id;
     }
 }
