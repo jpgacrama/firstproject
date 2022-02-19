a4e200e8389da1f0ec979cf3143ec0afea39a8db
Jonas Gacrama <gacrama.jonas@gmail.com>
2022-02-19 17:51:22 +0800

    About page is now made

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,4 +9,8 @@ class ProductsController extends Controller
     public function index() {
         return view('products.index');
     }
+
+    public function about() {
+        return 'About Us page';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use App\Http\Controllers\ProductsController;
 */
 
 Route::get('/products', [ProductsController::class, 'index']);
+Route::get('/products/about', [ProductsController::class, 'about']);
